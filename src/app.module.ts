c02d271671d89c70efed314dc6f13212e50d7eef
dynasty7777 <danilflorida77@gmail.com>
2026-02-12 18:54:25 +0200

    xuy4

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { DiscordService } from './discord/discord.service';
+
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    DiscordService,
+  ],
 })
 export class AppModule {}
